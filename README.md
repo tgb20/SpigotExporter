0c5d05bce2c3e7d07f4ae04f0d7f5d172d4ae387
Jacob Bashista <jacobbashista@jacobs-mac-mini.lan>
2021-12-19 19:19:26 -0500

    Switch to Node

--- README.md
+++ README.md
@@ -6,43 +6,20 @@ SpigotExporter will output metrics for each of your plugins and a cumulative dow
 
 Each plugin will have two metrics exposed:
 ```yml
-# HELP PLUGIN_NAME_downloads The total number of downloads for PLUGIN_NAME.
-# TYPE PLUGIN_NAME_downloads counter
-PLUGIN_NAME_downloads 3832
+# HELP spigot_PLUGIN_NAME_downloads The total number of downloads for PLUGIN_NAME.
+# TYPE spigot_PLUGIN_NAME_downloads counter
+spigot_PLUGIN_NAME_downloads 3832
 
-# HELP PLUGIN_NAME_rating The current rating for PLUGIN_NAME.
-# TYPE PLUGIN_NAME_rating gauge
-PLUGIN_NAME_rating 5
+# HELP spigot_PLUGIN_NAME_rating The current rating for PLUGIN_NAME.
+# TYPE spigot_PLUGIN_NAME_rating gauge
+spigot_PLUGIN_NAME_rating 5
 ```
 
 `PLUGIN_NAME` will be replaced with the name of your plugin where spaces are converted to _.
 
 The cumulative downloads metric:
 ```yml
-# HELP total_downloads The total number of downloads for this account.
-# TYPE total_downloads counter
-total_downloads 5909
+# HELP spigot_total_downloads The total number of downloads for this account.
+# TYPE spigot_total_downloads counter
+spigot_total_downloads 5909
 ```
-
-## How to use
-You can either use the live instance [spigot.onlinemo.de](https://spigot.onlinemo.de) or host it yourself. You can load the exporter page by going to /SPIGOT_USER_ID. 
-
-Example: To load my plugins [spigot.onlinemo.de/878178](https://spigot.onlinemo.de/878178)
-
-Once you have confirmed that the page loads correctly you can add it to your prometheus exporter:
-```yml
-- job_name: 'spigot'
-  metrics_path: /878178
-  static_configs:
-  - targets: ['spigot.onlinemo.de']
-```
-
-Replace `878178` with your spigot id
-
-### Self hosting
-Install requests:
-`pip install requests`
-
-Modify `serverPort` on line 5 to the port you want to use.
-
-I recommend setting up a reverse proxy but it is not necessary.
---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const JSONdb = require('simple-json-db');
+const axios = require('axios');
+const config = require('./config.json');
+const app = express();
+const port = config.port;
+
+const db = new JSONdb('storage.json');
+
+fetchAPIData = (async () => {
+    const spigotData = await axios.get('https://api.spigotmc.org/simple/0.1/index.php?action=getResourcesByAuthor&id=' + config.spigot_id);
+    let totalDownloads = 0;
+    spigotData.data.forEach(resource => {
+        totalDownloads += parseInt(resource.stats.downloads);
+    });
+    db.set('resources', spigotData.data);
+    db.set('total_downloads', totalDownloads);
+});
+
+fetchAPIData();
+
+setInterval(fetchAPIData, config.interval * 1000);
+
+app.get('/', (req, res) => {
+    res.send('Metrics are on /metrics');
+});
+
+app.get('/metrics', (req, res) => {
+    res.set('Content-Type', 'text/plain');
+    let responseString = '';
+    responseString += `# HELP spigot_total_downloads The total number of downloads for this account.\n# TYPE spigot_total_downloads counter\nspigot_total_downloads ${db.get('total_downloads')}\n\n`;
+    db.get('resources').forEach(resource => {
+        responseString += `# HELP spigot_${resource.title.replaceAll(' ', '_').replaceAll('-', '_')}_downloads The total number of downloads for this plugin.\n# TYPE spigot_${resource.title.replaceAll(' ', '_').replaceAll('-', '_')}_downloads counter\nspigot_${resource.title.replaceAll(' ', '_').replaceAll('-', '_')}_downloads ${resource.stats.downloads}\n\n`;
+        responseString += `# HELP spigot_${resource.title.replaceAll(' ', '_').replaceAll('-', '_')}_rating The rating for this plugin.\n# TYPE spigot_${resource.title.replaceAll(' ', '_').replaceAll('-', '_')}_rating counter\nspigot_${resource.title.replaceAll(' ', '_').replaceAll('-', '_')}_rating ${resource.stats.rating}\n\n`;
+    });
+    res.send(responseString);
+});
+
+app.listen(port, () => {
+    console.log(`SpigotExporter listening at http://localhost:${port}`);
+})
